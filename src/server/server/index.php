4dc753a013db4bc5ffe1fa02f1f0fcbe77fd4e8e
wolftankk <wolftankk@gmail.com>
2012-02-23 13:47:14 +0800

    改进跨域行为

--- src/server/server/index.php
+++ src/server/server/index.php
@@ -4,6 +4,12 @@
  * Configure Spyder directory
  */
 
+
+header("Access-Control-Allow-Headers: x-requested-with");
+header("Access-Control-Allow-Methods:POST, GET, OPTIONS");
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Max-Age:1728000");
+
 define("DS", DIRECTORY_SEPARATOR);
 define("SITE_ROOT", dirname(__FILE__).DS);
 define("MODULES_PATH", SITE_ROOT."modules".DS);
