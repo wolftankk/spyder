d45c85c03f826c39661cea12054ae5fac2490720
wolftankk <wolftankk@gmail.com>
2012-03-08 15:41:23 +0800

    add wp_insert_post method add PublicArticleToSite js method

--- src/server/client/js/libs/ArticleLib.js
+++ src/server/client/js/libs/ArticleLib.js
@@ -159,3 +159,43 @@ SpyderArticleSvc.prototype.DeleteArticle = function(AID, __callback){
 
 	Spyder.Ajax.asyncRequest("POST", this.url+method, __callbacks, Ext.JSON.encode(__message));
 }
+
+SpyderArticleSvc.prototype.PublicArticleToSite = function(AID, WID, __callback){
+	var method = "article.PublicArticleToSite";
+	var __message = {
+		method: method,
+		params: {
+			AID: AID,
+			WID: WID
+		}
+	}
+
+	var __callbacks = null
+	if (__callback){
+		__callbacks = {
+			callback: __callback,
+			success: function(o){
+				var __result = JSON.parse(o.responseText);
+				if (__result.error){
+					if ((typeof this.callback == "object") && this.callback.failure){
+						this.callback.failure(__result.error);
+					}
+				}else{
+					if ((typeof this.callback == "object") && this.callback.success){
+						this.callback.success(__result.result);
+					}else{
+						this.callback(__result.result);
+					}
+				}
+			},
+			failure: function(o){
+				if ((typeof this.callback == "object") && this.callback.failure){
+					this.callback.failure(o);
+				}
+			},
+			timeout: 3000
+		}
+	}
+
+	Spyder.Ajax.asyncRequest("POST", this.url+method, __callbacks, Ext.JSON.encode(__message));
+}
--- src/server/server/modules/article.php
+++ src/server/server/modules/article.php
@@ -140,21 +140,57 @@ class Article{
 			send_ajax_response("error", "This article #$aid is not found.");
 		}
 		//get website
-		$websiteData = $db->get_one("SELECT * FROM spyder.website_extra WHERE wid = '$wid'");
-		if (empty($websiteData) || !is_array($websiteData) || count($websiteData)){
-			send_ajax_response("error", "This website #$wid is not found.");
-		}
+		#$websiteData = $db->get_one("SELECT * FROM spyder.website_extra WHERE wid = '$wid'");
+		#if (empty($websiteData) || !is_array($websiteData) || count($websiteData)){
+		#	send_ajax_response("error", "This website #$wid is not found.");
+		#}
 
-		$method = $websiteData["method"];
-		//method: wordpress, dedecms, phpcms, ...
-		if ($method == "wordpress"){
-			//utf-8?
-			$siteDB = new Crow($websiteData["host"], $websiteData["name"], $websiteData["passwd"], $websiteData["dbname"]);
-			if ($siteDB){
-				//insert article data into wordpress
-			}
-		}else{
+		#$method = $websiteData["method"];
+		#//method: wordpress, dedecms, phpcms, ...
+		#if ($method == "wordpress"){
+		#	$this->wp_insert_post($articleData, $websiteData)
+		#}else{
 
+		#}
+	}
+
+	/**
+	 * Insert a post to wordpress
+	 *	$post = array(
+	 *	  'ID' => [ <post id> ] //Are you updating an existing post?
+	 *	  'menu_order' => [ <order> ] //If new post is a page, sets the order should it appear in the tabs.
+	 *	  'comment_status' => [ 'closed' | 'open' ] // 'closed' means no comments.
+	 *	  'ping_status' => [ 'closed' | 'open' ] // 'closed' means pingbacks or trackbacks turned off
+	 *	  'pinged' => [ ? ] //?
+	 *	  'post_author' => [ <user ID> ] //The user ID number of the author.
+	 *	  'post_category' => [ array(<category id>, <...>) ] //Add some categories.
+	 *	  'post_content' => [ <the text of the post> ] //The full text of the post.
+	 *	  'post_date' => [ Y-m-d H:i:s ] //The time post was made.
+	 *	  'post_date_gmt' => [ Y-m-d H:i:s ] //The time post was made, in GMT.
+	 *	  'post_excerpt' => [ <an excerpt> ] //For all your post excerpt needs.
+	 *	  'post_name' => [ <the name> ] // The name (slug) for your post
+	 *	  'post_parent' => [ <post ID> ] //Sets the parent of the new post.
+	 *	  'post_password' => [ ? ] //password for post?
+	 *	  'post_status' => [ 'draft' | 'publish' | 'pending'| 'future' ] //Set the status of the new post. 
+	 *	  'post_title' => [ <the title> ] //The title of your post.
+	 *	  'post_type' => [ 'post' | 'page' ] //Sometimes you want to post a page.
+	 *	  'tags_input' => [ '<tag>, <tag>, <...>' ] //For tags.
+	 *	  'to_ping' => [ ? ] //?
+	 *	);  
+	 * 
+	 */
+	private function wp_insert_post($articleData, $websiteData){
+		//utf-8?
+		$siteDB = new Crow($websiteData["host"], $websiteData["name"], $websiteData["passwd"], $websiteData["dbname"]);
+		$defaults = array(
+			'post_status' => "draf",
+			'post_type'   => "post",
+			'post_author' => 1,#defalt user id
+			'guid'		  => ""
+		);
+		if ($siteDB){
+			//insert article data into wordpress
+			
 		}
 	}
 }
