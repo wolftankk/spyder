77ba83af5b727e77d37ac37864a32e09b8e0d7f7
wolftankk <wolftankk@gmail.com>
2012-03-28 17:38:24 +0800

    update lib for realm

--- src/server/client/js/libs/RealmLib.js
+++ src/server/client/js/libs/RealmLib.js
@@ -323,4 +323,161 @@ SpyderRealmSvc.prototype.DeleteOperator = function(OID, __callback){
 }
 
 
+SpyderRealmSvc.prototype.GetRealmPageData = function(start, limit, AWhere, __callback){
+    var method = "realm.GetRealmPageData";
+    var __message = {
+        method: method,
+        params: {
+            Start: start,
+            Limit: limit,
+            AWhere: AWhere
+        }
+    }
 
+    var __callbacks = null
+    if (__callback){
+        __callbacks = {
+            callback: __callback,
+            success: function(o){
+                var __result = JSON.parse(o.responseText);
+                if (__result.error){
+                    if ((typeof this.callback == "object") && this.callback.failure){
+                        this.callback.failure(__result.error);
+                    }
+                }else{
+                    if ((typeof this.callback == "object") && this.callback.success){
+                        this.callback.success(__result.result);
+                    }else{
+                        this.callback(__result.result);
+                    }
+                }
+            },
+            failure: function(o){
+                if ((typeof this.callback == "object") && this.callback.failure){
+                    this.callback.failure(o);
+                }
+            },
+            timeout: 3000
+        }
+    }
+
+    Spyder.Ajax.asyncRequest("POST", this.url+method, __callbacks, Ext.JSON.encode(__message));
+}
+
+SpyderRealmSvc.prototype.AddRealm = function(RJSON, __callback){
+    var method = "realm.AddRealm";
+    var __message = {
+        method: method,
+        params: {
+	    RJSON: RJSON
+        }
+    }
+
+    var __callbacks = null
+    if (__callback){
+        __callbacks = {
+            callback: __callback,
+            success: function(o){
+                var __result = JSON.parse(o.responseText);
+                if (__result.error){
+                    if ((typeof this.callback == "object") && this.callback.failure){
+                        this.callback.failure(__result.error);
+                    }
+                }else{
+                    if ((typeof this.callback == "object") && this.callback.success){
+                        this.callback.success(__result.result);
+                    }else{
+                        this.callback(__result.result);
+                    }
+                }
+            },
+            failure: function(o){
+                if ((typeof this.callback == "object") && this.callback.failure){
+                    this.callback.failure(o);
+                }
+            },
+            timeout: 3000
+        }
+    }
+
+    Spyder.Ajax.asyncRequest("POST", this.url+method, __callbacks, Ext.JSON.encode(__message));
+}
+
+SpyderRealmSvc.prototype.EditRealm = function(RID, RJSON, __callback){
+    var method = "realm.EditRealm";
+    var __message = {
+        method: method,
+        params: {
+	    RID: RID,
+	    RJSON: RJSON
+        }
+    }
+
+    var __callbacks = null
+    if (__callback){
+        __callbacks = {
+            callback: __callback,
+            success: function(o){
+                var __result = JSON.parse(o.responseText);
+                if (__result.error){
+                    if ((typeof this.callback == "object") && this.callback.failure){
+                        this.callback.failure(__result.error);
+                    }
+                }else{
+                    if ((typeof this.callback == "object") && this.callback.success){
+                        this.callback.success(__result.result);
+                    }else{
+                        this.callback(__result.result);
+                    }
+                }
+            },
+            failure: function(o){
+                if ((typeof this.callback == "object") && this.callback.failure){
+                    this.callback.failure(o);
+                }
+            },
+            timeout: 3000
+        }
+    }
+
+    Spyder.Ajax.asyncRequest("POST", this.url+method, __callbacks, Ext.JSON.encode(__message));
+}
+
+SpyderRealmSvc.prototype.DeleteRealm = function(RID, __callback){
+    var method = "realm.DeleteRealm";
+    var __message = {
+        method: method,
+        params: {
+	    RID : RID
+        }
+    }
+
+    var __callbacks = null
+    if (__callback){
+        __callbacks = {
+            callback: __callback,
+            success: function(o){
+                var __result = JSON.parse(o.responseText);
+                if (__result.error){
+                    if ((typeof this.callback == "object") && this.callback.failure){
+                        this.callback.failure(__result.error);
+                    }
+                }else{
+                    if ((typeof this.callback == "object") && this.callback.success){
+                        this.callback.success(__result.result);
+                    }else{
+                        this.callback(__result.result);
+                    }
+                }
+            },
+            failure: function(o){
+                if ((typeof this.callback == "object") && this.callback.failure){
+                    this.callback.failure(o);
+                }
+            },
+            timeout: 3000
+        }
+    }
+
+    Spyder.Ajax.asyncRequest("POST", this.url+method, __callbacks, Ext.JSON.encode(__message));
+}
