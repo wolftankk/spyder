788f513dd9959008e0f1fec8f6c78cc301b2766f
wolftankk <wolftankk@gmail.com>
2012-05-10 15:13:29 +0800

    update git ignore file

--- src/spyder/config.py
+++ src/spyder/config.py
@@ -4,14 +4,14 @@ DBCONFIG = {
     "host" : "172.16.130.7",
     "user" : "root",
     "passwd" : "",
-    "dbname" : "spyder"
+    "dbname" : "wow_spyder"
 }
 
 #staticPath = "/Users/wolftankk/home/workspace/spyder/static"
 
 # where will these images be saved
 # Choose: locale / remote
-storeImage = True
+storeImage = False
 imageSaveMethod = "remote"
 uploadPath = "http://172.16.130.7/static/upload_image.php"
 staticUrl = "http://res.bigamer.tw/"
