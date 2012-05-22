5d2ec17c4db8bced18c6aeaa5ffe70b617a1c323
wolftankk <wolftankk@gmail.com>
2012-05-22 17:55:54 +0800

    add images

--- src/server/server/include/supesite.php
+++ src/server/server/include/supesite.php
@@ -72,7 +72,7 @@ class Supesite {
     }
 
     public function insert_article($articleData, $gameid=0){
-	$hash = substr(md5($articleData["url"]), 0, 10);
+	$hash = substr(md5($articleData["url"]), 0, 16);
 
 	//check
 	$isExist = $this->checkArticleExist($hash, $articleData["title"]);
@@ -120,18 +120,54 @@ class Supesite {
 	    $setsqlarr["gameid"] = $gameid;
 	}
 
+	$images = $articleData["images"];
+	if (!empty($images)){
+	    $images = unserialize($images);
+	}
+
+
 	//styletitle 标题样式
 	$setsqlarr["haveattach"] = 0;
+	if (is_array($images) && count($images) > 0){
+	    $setsqlarr["haveattach"] = 1;
+	}
 	$setsqlarr["dateline"]   = time();
 
 	$itemid = $this->inserttable("postitems", $setsqlarr, 1);
+
+	if (is_array($images) && count($images) > 0){
+	    for ($i = 0; $i < count($images); $i++){
+		$image = $images[$i];
+		//get path, name, type
+		$image = str_replace("http://res.bigamer.tw/", "", $image);
+		$img_path = $image;
+		$image_info = explode("/", $image);
+		$image_file = end($image_info);
+		list($image_name, $image_type) = explode(".", $image_file);
+		$imgsql = array(
+		    'isavailable' => 1,
+		    'type' => 'news',
+		    'uid' => $pUserID,
+		    'itemid' => $itemid,
+		    'catid' => $this->defaultCatID,
+		    'dateline' => time(),
+		    'filename' => $image_file,
+		    'subject' => $image_name,
+		    'attachtype' => $image_type,
+		    'isimage' => 1,
+		    'size' => 4000,
+		    'filepath' => $img_path,
+		    'hash' => $hash
+		);
+		$this->inserttable('attachments', $imgsql, 1);
+	    }
+	}
 
 	//插入到spacenews
 	$setsqlarr = array(
 	    'message' => mysql_escape_string($articleData['content']),
 	    'postip'  => '127.0.0.1'
 	);
-
 	//TAG line360
 	//include tags
 	$setsqlarr["itemid"] = $itemid;
@@ -139,7 +175,7 @@ class Supesite {
 	$nid = $this->inserttable("postmessages", $setsqlarr, 1);
 
 	if ($nid > 0){
-	    #send_ajax_response("success", true);
+	//    #send_ajax_response("success", true);
 	    return $nid;
 	}
     }
