a22713808dd873cfb9529e4b2e6634549ad1c493
wolftankk <wolftankk@gmail.com>
2012-12-13 01:19:09 +0800

    add add/edit/view/list/delete

--- spyder/web/views/seed.py
+++ spyder/web/views/seed.py
@@ -2,3 +2,27 @@ from flask import Module, url_for, g
 
 seed = Module(__name__)
 
+@seed.route("/add/", methods=("GET", "POST"))
+@auth.require(401)
+def add():
+    """
+
+    """
+
+@seed.route("/view/<int:seed_id>/")
+def view(action, seed_id):
+    return seed_id
+
+
+@seed.route("/edit/<int:seed_id>/")
+def edit(action, seed_id):
+    return seed_id
+
+@seed.route("/delete/<int:seed_id>")
+def delete(action, seed_id):
+    return seed_id
+
+@seed.route("/")
+@seed.route("/list/<int:page>/")
+def list(action, page=1):
+    return "list"
