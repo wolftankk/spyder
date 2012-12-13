123eac9f90b60e2f187b36cce4ae9f0548bdeeed
fireyy <fireyy@gmail.com>
2012-12-13 18:45:22 +0800

    spinne 模板添加

--- spyder/web/views/seeds.py
+++ spyder/web/views/seeds.py
@@ -1,8 +1,9 @@
 from flask import Module, url_for, g
+from flask import render_template
 
 seeds = Module(__name__)
 
 @seeds.route("/")
 @seeds.route("/<int:page_id>")
 def index(page=1):
-    return "seed lists"
+    return render_template("seed/list.html")
