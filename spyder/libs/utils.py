fe9b1a77a0f2b8792cc10f16863eff27f73c2804
wolftankk <wolftankk@gmail.com>
2012-12-14 14:58:23 +0800

    update db

--- spyder/libs/utils.py
+++ spyder/libs/utils.py
@@ -2,7 +2,158 @@
 
 import itertools
 
-__all__ = ['safestr', 'safeunicode']
+__all__ = ['safestr', 'safeunicode', 'ThreadedDict', 'threadeddict', 'storage', 'Storage', 'iters']
+
+import sys
+from threading import local as threadlocal
+
+class Storage(dict):
+    """
+    A Storage object is like a dictionary except `obj.foo` can be used
+    in addition to `obj['foo']`.
+    >>> o = storage(a=1)
+    >>> o.a
+    1
+    >>> o['a']
+    1
+    >>> o.a = 2
+    >>> o['a']
+    2
+    >>> del o.a
+    >>> o.a
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'a'
+    """
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError, k:
+            raise AttributeError, k
+    
+    def __setattr__(self, key, value):
+        self[key] = value
+    
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError, k:
+            raise AttributeError, k
+    
+    def __repr__(self):
+        return '<Storage ' + dict.__repr__(self) + '>'
+
+storage = Storage
+
+iters = [list, tuple]
+import __builtin__
+if hasattr(__builtin__, 'set'):
+    iters.append(set)
+if hasattr(__builtin__, 'frozenset'):
+    iters.append(set)
+if sys.version_info < (2,6): # sets module deprecated in 2.6
+    try:
+        from sets import Set
+        iters.append(Set)
+    except ImportError:
+        pass
+    
+class _hack(tuple): pass
+iters = _hack(iters)
+iters.__doc__ = ""
+
+class ThreadedDict(threadlocal):
+    """
+    >>> d = ThreadedDict()
+    >>> d.x = 1
+    >>> d.x
+    >>> 1
+    >>> import thread
+    >>> def f(): d.x = 2
+
+    >>> t = threading.Thread(target=f)
+    >>> t.start()
+    >>> t.join();
+    >>> d.x
+    >>> 1
+    """
+    _instance = set()
+
+    def __init__(self):
+	ThreadedDict._instance.add(self)
+
+    def __del__(self):
+	ThreadedDict._instance.remove(self)
+
+    def __hash__(self):
+	return id(self)
+
+    def clear_all():
+	for t in list(ThreadedDict._instance):
+	    t.clear()
+
+    clear_all = staticmethod(clear_all)
+    def __getitem__(self, key):
+	return self.__dict__[key]
+
+    def __setitem__(self, key, value):
+	self.__dict__[key] = value
+
+    def __delitem__(self, key):
+	del self.__dict__[key]
+
+    def __contains__(self, key):
+	return key in self.__dict__
+
+    has_key = __contains__
+
+    def clear(self):
+	self.__dict__.clear()
+
+    def copy(self):
+	return self.__dict__.copy()
+
+    def get(self, key, default=None):
+	return self.__dict__.get(key, default)
+
+    def items(self):
+	return self.__dict__.items()
+
+    def iteritems(self):
+	return self.__dict__.iteritems()
+
+    def keys(self):
+	return self.__dict__.keys()
+
+    def iterkeys(self):
+	return self.__dict__.iterkeys()
+
+    iter = iterkeys
+
+    def values(self):
+	return self.__dict__.values()
+
+    def itervalues(self):
+	return self.__dict__.itervalues()
+
+    def pop(self, key, *args):
+	return self.__dict__.pop(key, *args)
+
+    def popitem(self):
+	return self.__dict__.popitem()
+
+    def setdefault(self, key, default=None):
+	return self.__dict__.setdefault(key, default)
+
+    def update(self, *args, **kwargs):
+	self.__dict__.update(*args, **kwargs)
+
+    def __repr__(self):
+	return '<ThreadedDict %r>' % self.__dict__
+
+    __str__ = __repr__
+
+threadeddict = ThreadedDict
 
 def safestr(obj, encoding="utf-8"):
     if isinstance(obj, unicode):
