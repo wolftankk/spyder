f301f0109327f3030eb916b1723c5ee96238cc1d
fireyy <fireyy@gmail.com>
2012-12-21 17:54:04 +0800

    数据库字段更新: 添加 seed_Type varchar类型

--- spyder.sql
+++ spyder.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: local.site
--- 生成日期: 2012 年 12 月 21 日 13:53
+-- 生成日期: 2012 年 12 月 21 日 17:52
 -- 服务器版本: 5.5.21
 -- PHP 版本: 5.3.15
 
@@ -33,8 +33,8 @@ CREATE TABLE IF NOT EXISTS `field_template` (
   `title` varchar(64) NOT NULL COMMENT 'nickname',
   `type` enum('article','game','kaifu','kaice','gift','company') NOT NULL DEFAULT 'article',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `sid` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+  UNIQUE KEY `name` (`name`,`type`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
 
 --
 -- 转存表中的数据 `field_template`
@@ -46,7 +46,21 @@ INSERT INTO `field_template` (`id`, `name`, `title`, `type`) VALUES
 (3, 'tag', '标签', 'article'),
 (4, 'source', '来源', 'article'),
 (5, 'content', '正文', 'article'),
-(6, 'create_time', '发表时间', 'article');
+(6, 'create_time', '发表时间', 'article'),
+(7, 'game_name', '游戏名称', 'game'),
+(8, 'game_tag', '游戏模式', 'game'),
+(9, 'game_theme', '游戏题材', 'game'),
+(10, 'game_effect', '画面方式', 'game'),
+(11, 'game_fight', '战斗方式', 'game'),
+(12, 'game_area', '游戏产地', 'game'),
+(13, 'game_price', '收费模式', 'game'),
+(14, 'game_status', '游戏状态', 'game'),
+(15, 'game_developer', '开发公司', 'game'),
+(16, 'game_intro', '游戏介绍', 'game'),
+(17, 'game_website', '官网地址', 'game'),
+(18, 'game_thumb', '游戏缩略图', 'game'),
+(19, 'game_avatar', '游戏封面', 'game'),
+(20, 'create_time', '插入时间', 'game');
 
 -- --------------------------------------------------------
 
@@ -65,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `seeds` (
   `enabled` tinyint(1) NOT NULL DEFAULT '1',
   `listtype` enum('html','feed') NOT NULL,
   `rule` mediumtext NOT NULL,
-  `frequency` tinyint(3) unsigned NOT NULL,
+  `frequency` int(10) unsigned NOT NULL,
   `timeout` smallint(6) NOT NULL DEFAULT '300',
   `tries` tinyint(4) NOT NULL DEFAULT '5',
   `created_time` int(11) NOT NULL,
@@ -74,15 +88,15 @@ CREATE TABLE IF NOT EXISTS `seeds` (
   `finish_time` int(11) NOT NULL,
   PRIMARY KEY (`sid`),
   UNIQUE KEY `url` (`base_url`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 --
 -- 转存表中的数据 `seeds`
 --
 
 INSERT INTO `seeds` (`sid`, `seed_name`, `type`, `base_url`, `charset`, `lang`, `enabled`, `listtype`, `rule`, `frequency`, `timeout`, `tries`, `created_time`, `update_time`, `start_time`, `finish_time`) VALUES
-(2, 'kaifu新闻', 'article', NULL, 'utf-8', 'zhCN', 1, 'html', 'a:10:{s:7:"maxpage";s:1:"5";s:4:"step";s:2:"10";s:9:"urlformat";s:53:"http://www.kaifu.com/article-24--0-0-0-0-0-0-(*).html";s:7:"urltype";s:10:"createLink";s:7:"filters";s:0:"";s:13:"contentparent";s:57:"div[class=''fl newsinfo_topline boder_base newsinfo_left'']";s:9:"startpage";s:1:"0";s:10:"pageparent";s:0:"";s:10:"listparent";s:57:"div[class=''fl newsinfo_topline boder_base newsinfo_left'']";s:11:"entryparent";s:18:"li[class=''b_line'']";}', 255, 30, 5, 0, 1356065567, 0, 0),
-(6, '34234', 'article', NULL, 'gb2312', 'zhCN', 0, 'html', 'a:10:{s:7:"maxpage";s:0:"";s:4:"step";s:0:"";s:9:"urlformat";s:5:"34234";s:7:"urltype";s:9:"inputLink";s:7:"filters";s:0:"";s:13:"contentparent";s:3:"234";s:9:"startpage";s:0:"";s:10:"pageparent";s:0:"";s:10:"listparent";s:4:"2342";s:11:"entryparent";s:5:"23423";}', 1, 30, 5, 1356065619, 1356065619, 0, 0);
+(2, 'kaifu新闻', 'article', NULL, 'utf-8', 'zhCN', 0, 'html', 'a:11:{s:9:"urlformat";s:53:"http://www.kaifu.com/article-24--0-0-0-0-0-0-(*).html";s:10:"pageparent";s:0:"";s:7:"maxpage";s:1:"5";s:4:"step";s:2:"10";s:7:"filters";s:0:"";s:10:"contenturl";s:75:"div[class=''fl p14 blue news_detailliset lh40''] a[class=''blue''].attr(''href'')";s:10:"listparent";s:57:"div[class=''fl newsinfo_topline boder_base newsinfo_left'']";s:7:"urltype";s:10:"createLink";s:13:"contentparent";s:57:"div[class=''fl newsinfo_topline boder_base newsinfo_left'']";s:9:"startpage";s:1:"0";s:11:"entryparent";s:18:"li[class=''b_line'']";}', 7200, 30, 5, 0, 1356076213, 0, 0),
+(7, '开服网游戏', 'game', NULL, 'auto', 'zhCN', 0, 'html', 'a:11:{s:9:"urlformat";s:60:"http://www.kaifu.com/gamelist-1-0-0-0-0-0-0-0-0-0-1-(*).html";s:10:"pageparent";s:0:"";s:7:"maxpage";s:2:"28";s:4:"step";s:2:"64";s:7:"filters";s:0:"";s:10:"contenturl";s:0:"";s:10:"listparent";s:28:"div[class=''box_line picbox'']";s:7:"urltype";s:10:"createLink";s:13:"contentparent";s:50:"div[class=''fl boder_base newsinfo_left game_info'']";s:9:"startpage";s:1:"0";s:11:"entryparent";s:27:"ul[class=''position_div fl'']";}', 3600, 30, 5, 1356075535, 1356076153, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -107,15 +121,24 @@ CREATE TABLE IF NOT EXISTS `seed_fields` (
 INSERT INTO `seed_fields` (`seed_id`, `field_id`, `value`, `page_type`) VALUES
 (2, 1, 'h6[class=''lh40'']', 'content'),
 (2, 2, 'span[class=author]', 'content'),
-(2, 3, 'span[class=tag]', 'list'),
+(2, 3, 'span[class=''gray decoration''] >  a', 'list'),
 (2, 5, 'div[class=''newsinfo_artical p14 lh24'']', 'content'),
-(2, 4, '', 'content'),
-(6, 1, '23423', 'content'),
-(6, 2, '234234', 'content'),
-(6, 3, '234234', 'list'),
-(6, 4, '2342', 'content'),
-(6, 5, '23423', 'list'),
-(6, 6, '', 'content');
+(2, 4, '来源：(*)', 'content'),
+(2, 6, '本文 (*) 发布', 'list'),
+(7, 7, 'div[class=''title''] > h6[class=''fl'']', 'content'),
+(7, 8, '<span>游戏模式：<a href="(*)" target="_blank">[参数]</a></span>', 'content'),
+(7, 9, '<span>游戏题材：<a href="(*)" target="_blank">[参数]</a></span>', 'content'),
+(7, 10, '<span>画面方式：<a href="(*)" target="_blank">[参数]</a></span>', 'content'),
+(7, 11, '<span>战斗方式：<a href="(*)" target="_blank">[参数]</a></span>', 'content'),
+(7, 12, '<span>游戏产地：<a href="(*)" target="_blank">[参数]</a></span>', 'content'),
+(7, 13, '<span>收费模式：<a href="(*)" target="_blank">[参数]</a></span>', 'content'),
+(7, 14, '', 'content'),
+(7, 15, '<span>开发公司：<a href="(*)" target="_blank">[参数]</a></span>', 'content'),
+(7, 16, 'div[class=''newsinfo_artical p14 lh24 fl mt_5''] p', 'content'),
+(7, 17, 'a[class=''bt_playgame''].attr(''href'')', 'content'),
+(7, 18, 'img[class=''imgboxs''].attr(''src'')', 'list'),
+(7, 19, 'div[class=''fl pic mr picbd''] img.attr(''src'')', 'content'),
+(7, 20, 'p[class=''gray'']', 'list');
 
 -- --------------------------------------------------------
 
