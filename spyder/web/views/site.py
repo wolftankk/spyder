93a7d4c632ee4bb16def41701c0d2d003eace9e2
fireyy <fireyy@gmail.com>
2012-12-25 15:53:54 +0800

    增加图片上传接口：FTP/阿里云; FTP测试连接

--- spyder/web/views/site.py
+++ spyder/web/views/site.py
@@ -7,6 +7,7 @@ from web.helpers import auth
 from web.models import Site, Field
 
 import MySQLdb
+from ftplib import FTP
 
 site = Module(__name__)
 
@@ -31,11 +32,19 @@ def add():
             site = Site(current_app)
             sync_profile = {
                 "staticUrl": request.form.get("staticUrl"),
+                "staticType": request.form.get("staticType"),
                 "mysql_server": request.form.get("mysql_server"),
                 "mysql_dbname": request.form.get("mysql_dbname"),
                 "mysql_prefix": request.form.get("mysql_prefix"),
                 "mysql_username": request.form.get("mysql_username"),
                 "mysql_password": request.form.get("mysql_password"),
+                "ftp_server": request.form.get("ftp_server"),
+                "ftp_port": request.form.get("ftp_port"),
+                "ftp_path": request.form.get("ftp_path"),
+                "ftp_username": request.form.get("ftp_username"),
+                "ftp_password": request.form.get("ftp_password"),
+                "access_id": request.form.get("access_id"),
+                "secret_access_key": request.form.get("secret_access_key"),
                 "api_url": request.form.get("api_url")
             }
             sync_profile = phpserialize.dumps(sync_profile)
@@ -66,6 +75,27 @@ def test_mysql():
             return "0"
     return "0"
 
+@site.route("/test_ftp/", methods=("GET", "POST"))
+@auth
+def test_ftp():
+    mess = None
+    if request.method == "POST":
+        ftp_server = request.form.get("ftp_server")
+        ftp_port = int(request.form.get("ftp_port"))
+        ftp_username = request.form.get("ftp_username")
+        ftp_password = request.form.get("ftp_password")
+        ftp_path = request.form.get("ftp_path")
+        ftp = FTP()
+        try:
+            ftp.connect(host=ftp_server,port=ftp_port,timeout=30)
+            ftp.login(user=ftp_username,passwd=ftp_password)
+            ftp.cwd(ftp_path)
+            mess = u"FTP连接成功"
+        except:
+            mess = u"FTP连接失败"
+        ftp.quit()
+    return mess
+
 @site.route("/edit/<int:site_id>/", methods=("GET", "POST"))
 @auth
 def edit(site_id):
@@ -85,11 +115,19 @@ def edit(site_id):
             site = Site(current_app)
             sync_profile = {
                 "staticUrl": request.form.get("staticUrl"),
+                "staticType": request.form.get("staticType"),
                 "mysql_server": request.form.get("mysql_server"),
                 "mysql_dbname": request.form.get("mysql_dbname"),
                 "mysql_prefix": request.form.get("mysql_prefix"),
                 "mysql_username": request.form.get("mysql_username"),
                 "mysql_password": request.form.get("mysql_password"),
+                "ftp_server": request.form.get("ftp_server"),
+                "ftp_port": request.form.get("ftp_port"),
+                "ftp_path": request.form.get("ftp_path"),
+                "ftp_username": request.form.get("ftp_username"),
+                "ftp_password": request.form.get("ftp_password"),
+                "access_id": request.form.get("access_id"),
+                "secret_access_key": request.form.get("secret_access_key"),
                 "api_url": request.form.get("api_url")
             }
             sync_profile = phpserialize.dumps(sync_profile)
