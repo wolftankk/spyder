09b71590af76db4a10bdfe3a17821c98f238267f
wolftankk <wolftankk@gmail.com>
2013-11-25 10:32:18 +0800

    add vim local setting

--- src/spyder/__init__.py
+++ src/spyder/__init__.py
@@ -1,11 +1,7 @@
 #coding: utf-8
 
 '''
-Local variables:
-tab-width: 4
-c-basic-offset: 4
-End:
-vim600: sw=4 ts=8
+vim: ts=8
 '''
 
 import os, sys
@@ -30,49 +26,49 @@ from threading import Thread;
 
 class scrapy(Thread):
     def __init__(self, seed):
-	Thread.__init__(self)
-	#self.daemon = True;
+        Thread.__init__(self)
+        #self.daemon = True;
 
-	#初始化
-	self.sid = seed["sid"]
-	self.frequency = seed["frequency"]
-	self.starttime = seed["start_time"]
-	self.finishtime = seed["finish_time"]
-	self.starttime = now()
-	self.seed = Seed(seed)
+        #初始化
+        self.sid = seed["sid"]
+        self.frequency = seed["frequency"]
+        self.starttime = seed["start_time"]
+        self.finishtime = seed["finish_time"]
+        self.starttime = now()
+        self.seed = Seed(seed)
 
     def _crab(self):
-	self.starttime = now();
-	
-	try:
-	    g = Grab(self.seed)
-	    #发布到外网
-	    g.push()
-	    self.finishtime = now()
-	    seed_db.edit(self.sid, **{"start_time": self.starttime, "finish_time": self.finishtime})
-	    log_db.insert(**{"sid" : self.sid, "start_time" : self.starttime, "finish_time" : self.finishtime, "`status`" : 1, "message" : "采集成功"})
-	    return True
-	except Exception, e:
-	    log_db.insert(**{"sid" : self.sid, "start_time" : self.starttime, "finish_time" : now(), "`status`" : 0, "message" : "采集失败, 原因:" + str(e)})
-	    return False
+        self.starttime = now();
+
+        try:
+            g = Grab(self.seed)
+            #发布到外网
+            g.push()
+            self.finishtime = now()
+            seed_db.edit(self.sid, **{"start_time": self.starttime, "finish_time": self.finishtime})
+            log_db.insert(**{"sid" : self.sid, "start_time" : self.starttime, "finish_time" : self.finishtime, "`status`" : 1, "message" : "采集成功"})
+            return True
+        except Exception, e:
+            log_db.insert(**{"sid" : self.sid, "start_time" : self.starttime, "finish_time" : now(), "`status`" : 0, "message" : "采集失败, 原因:" + str(e)})
+            return False
 
     def run(self):
-	while True:
-	    if ((self.finishtime + self.frequency) <= now()):
-		self._crab();
-	    else:
-		need_sleep_sec = (self.finishtime + self.frequency) - now();
-		time.sleep(need_sleep_sec);
-		self._crab();
+        while True:
+            if ((self.finishtime + self.frequency) <= now()):
+                self._crab();
+            else:
+                need_sleep_sec = (self.finishtime + self.frequency) - now();
+                time.sleep(need_sleep_sec);
+                self._crab();
 
 def run():
     r = seed_db.select()
     if (len(r)):
-	seedList = r.list()
-	#分配线程池, 每个都是独立管理的
-	for seed in seedList:
-	    s = scrapy(seed)
-	    s.start();
+        seedList = r.list()
+        #分配线程池, 每个都是独立管理的
+        for seed in seedList:
+            s = scrapy(seed)
+            s.start();
 
 if __name__ == "__main__":
     run()
--- src/spyder/fetch.py
+++ src/spyder/fetch.py
@@ -1,11 +1,7 @@
 #coding: utf-8
 
 '''
-Local variables:
-tab-width: 4
-c-basic-offset: 4
-End:
-vim600: sw=4 ts=8
+vim: ts=8
 '''
 
 import os, sys
